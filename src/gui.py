d686282c1a7c1b3f4f9825a3b3902ab6aa14b406
aysxploit <ayesha.imran.a346@outlook.com>
2025-09-21 18:51:53 +0500

    update app front-end

---
+++ src/gui.py
@@ -0,0 +1,5 @@
+def test_imports():
+    import src.alpr as _  # noqa: F401
+    import src.db as _    # noqa: F401
+    import src.gemini_client as _  # noqa: F401
+    import src.logging_setup as _  # noqa: F401
